83eba606c89f07889208f9b07be08f2ae8cbbc9c
m-eleoterio <m.eleoteriovieira@gmail.com>
2024-07-09 21:39:07 -0300

    feat: cria migrate de task e seus relacionamentos

---
+++ app/Models/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title','description'
+    ];
+
+    public function user() {
+        return belongsTo(User::class);
+    }
+}
